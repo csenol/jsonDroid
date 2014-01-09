a72e73b01addc9f576f2132640d22df6f6d207ce
Çağdaş Şenol <cagdas@vngrs.com>
2014-01-09 14:59:15 +0200

    Add some tests

---
+++ json/src/test/scala/csenol/json/JsonDroidSpec.scala
@@ -0,0 +1,101 @@
+package csenol.json.test
+
+import csenol.json._
+import csenol.json.DefaultJsonProtocols._
+
+import collection.mutable.Stack
+import org.scalatest._
+import java.io.{ StringWriter, StringReader }
+import scala.util.Try
+import Implicits._
+
+class SimpleFormatSpec extends FlatSpec with Matchers {
+  
+  val s = Simple(1, 3.14, 12345L, "simple", true)
+  val sString = """{"i":1,"d":3.14,"l":12345,"s":"simple","b":true}"""
+
+  "JsonReader" should "produce correct case class from json string" in {
+    val jReader = new JsonReader( new StringReader( sString))
+    assert( Option(s) === jReader.getAs[Simple])
+  }
+
+  "JsonWriter" should "produce correct json string from case class" in {
+    val sWriter = new StringWriter()
+    val jWriter = new JsonWriter( sWriter)
+    jWriter.write(s)
+    val json = sWriter.toString
+    assert(sString === json)
+  }
+}
+
+
+
+class ComplexFormatSpec extends FlatSpec with Matchers {
+
+  val s = Simple(1, 3.14, 12345L, "simple", true)
+  val sString = """{"i":1,"d":3.14,"l":12345,"s":"simple","b":true}"""
+
+  val complexFull = Complex(
+    i = 1,
+    s = s,
+    sopt = Some(s),
+    lInt = Vector(1,2),
+    lSimple = Vector(s),
+    lOptSimple = Vector(Some(s)),
+    optListSimple = Some(Vector(s))
+  )
+
+  val complexPartial = Complex(
+    i = 1,
+    s = s,
+    sopt = None,
+    lInt = Vector.empty[Int],
+    lSimple = Vector.empty[Simple],
+    lOptSimple = Vector.empty[Option[Simple]],
+    optListSimple = None
+)
+
+  "JsonFormat" should "produce correct json string from complex case class" in {
+    val sWriter = new StringWriter()
+    val jWriter = new JsonWriter( sWriter)
+    jWriter.write(complexFull)
+    val json = sWriter.toString
+    val cString = """{"i":1,"s":{"i":1,"d":3.14,"l":12345,"s":"simple","b":true},"sopt":{"i":1,"d":3.14,"l":12345,"s":"simple","b":true},"lInt":[1,2],"lSimple":[{"i":1,"d":3.14,"l":12345,"s":"simple","b":true}],"lOptSimple":[{"i":1,"d":3.14,"l":12345,"s":"simple","b":true}],"optListSimple":[{"i":1,"d":3.14,"l":12345,"s":"simple","b":true}]}"""
+    assert(cString === json)
+    val jReader = new JsonReader( new StringReader ( cString))
+    jReader.setLenient(true)
+    assert( Try(complexFull) === jReader.getAsTry[Complex])
+    
+    
+  }
+  
+  "JsonWriter" should "produce correct json string from partial complex case class" in {
+    val sWriter = new StringWriter()
+    val jWriter = new JsonWriter( sWriter)
+    jWriter.write(complexPartial)
+    val json = sWriter.toString
+    val cString = """{"i":1,"s":{"i":1,"d":3.14,"l":12345,"s":"simple","b":true},"lInt":[],"lSimple":[],"lOptSimple":[]}"""
+    assert(cString === json)
+    val jReader = new JsonReader( new StringReader ( cString))
+    jReader.setLenient(true)
+    assert( Try(complexPartial) === jReader.getAsTry[Complex])
+  }
+
+  "JsonFormat" should "work for top level Lists as well" in {
+    val s1 = Simple(1, 3.14, 12345L, "simple1", true)
+    val s2 = Simple(2, 3.14, 12345L, "simple2", true)
+    val sString = """[{"i":1,"d":3.14,"l":12345,"s":"simple1","b":true},{"i":2,"d":3.14,"l":12345,"s":"simple2","b":true}]"""
+    val sWriter = new StringWriter()
+    val jWriter = new JsonWriter( sWriter)
+    jWriter.write(Vector(s1, s2))
+    val json = sWriter.toString
+    assert(sString === json )
+
+    val jReader = new JsonReader( new StringReader ( sString))
+    assert( Try(Vector(s1, s2)) === jReader.getAsTry[Vector[Simple]])
+    
+  }
+
+
+
+}
---
+++ json/src/test/scala/csenol/json/package.scala
@@ -0,0 +1,72 @@
+package csenol.json
+
+import csenol.json._
+import csenol.json.DefaultJsonProtocols._
+
+
+package object test {
+  case class Simple(i: Int, d: Double, l: Long, s: String, b :Boolean)
+  case class Complex(i: Int, s: Simple, sopt: Option[Simple], lInt: Vector[Int], 
+                   lSimple: Vector[Simple], lOptSimple: Vector[Option[Simple]], optListSimple: Option[Vector[Simple]])
+
+  object Implicits {
+
+    implicit object SimpleFormat extends JsonFormat[Simple] {
+      def read(in: JsonReader): Simple = {
+        var defSimple = Simple(0 , 0.0 , 0L , "" , false)
+        while(in.hasNext ){
+          val name = in.nextName
+          name match {
+            case "i" => defSimple = defSimple.copy(i = in.getAs[Int].get)
+            case "d" => defSimple = defSimple.copy(d = in.getAs[Double].get)
+            case "l" => defSimple = defSimple.copy(l = in.getAs[Long].get)
+            case "s" => defSimple = defSimple.copy(s = in.getAs[String].get)
+            case "b" => defSimple = defSimple.copy(b = in.getAs[Boolean].get)
+          }
+        }
+        defSimple
+      }
+
+      def write(t: Simple, out: JsonWriter): JsonWriter = {
+        out.write("i", t.i)
+        out.write("d", t.d)
+        out.write("l", t.l)
+        out.write("s", t.s)
+        out.write("b", t.b)
+      }
+    }
+  
+
+    implicit object ComplexFormat extends JsonFormat[Complex] {
+      def read(in: JsonReader): Complex = {
+        var defComplex = Complex(1, null, None, null, null, null, None)
+        while(in.hasNext) {
+          val name = in.nextName
+          name match {
+            case "i" => defComplex = defComplex.copy(i = in.getAs[Int].get)
+            case "s" => defComplex = defComplex.copy(s = in.getAs[Simple].get)
+            case "sopt" => defComplex = defComplex.copy(sopt = in.getAs[Option[Simple]].get)
+            case "lInt" => defComplex = defComplex.copy(lInt = in.getAs[Vector[Int]].get)
+            case "lSimple" => defComplex = defComplex.copy(lSimple = in.getAs[Vector[Simple]].get)
+            case "lOptSimple" => defComplex = defComplex.copy(lOptSimple = in.getAs[Vector[Option[Simple]]].get)
+            case "optListSimple" => defComplex = defComplex.copy(optListSimple = in.getAs[Option[Vector[Simple]]].get)
+            case _ => {in.skipValue}                                                     
+          }
+        }
+        defComplex
+      }
+      
+      def write(t: Complex, out: JsonWriter): JsonWriter = {
+        out.write("i", t.i)
+        out.write("s", t.s)
+        out.write("sopt", t.sopt)
+        out.write("lInt", t.lInt)
+        out.write("lSimple", t.lSimple)
+        out.write("lOptSimple", t.lOptSimple)
+        out.write("optListSimple", t.optListSimple)
+      }
+      
+    }
+  }
+  
+}
